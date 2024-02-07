3ef449dc61988816ff40d44c054fbe31f2a8c5d4
AdielTuson <adielt81@gmail.com>
2024-02-07 18:37:15 +0200

    Add a placeMark function to gameBoard
    this function will accept a user row and column and mark
    arguments and will place the mark in the correct place at
    the board array.

--- script.js
+++ script.js
@@ -1,43 +1,3 @@
-const Gameboard = (function () {
-    const rows = 3;
-    const columns = 3;
-    const board = [];
-    
-    //Will populate the board array
-    for (let i = 0; i < rows; i++) {
-        board[i] = [];
-        for (let j = 0; j < columns; j++) {
-            board[i].push("x");
-        }
-    }
-    
-    //Method for getting entire board for our UI
-    const getBoard = () => board;
-
-
-    const placeMark = (row, column, player) => {
-
-    }
-
-    return { board };
-})();
-
-//A function that will change the value of the current space in the board
-function setSpaceValue() {
-    let value = "";
-
-    const addMark = (playerMark) => {
-        value = playerMark;
-    }
-
-    const getValue = () => value;
-
-    return {
-        addMark,
-        getValue
-    };
-}
-
 const createPlayer = (name, symbol) => {
     return { 
         name, 
@@ -48,6 +8,59 @@ const playerOne = createPlayer('player one', 'X');
 const playerTwo = createPlayer('player two', 'O');
 
 
+const Gameboard = (function () {
+    const rows = 3;
+    const columns = 3;
+    const board = [];
+    
+    //Will populate the board array
+    for (let i = 0; i < rows; i++) {
+        board[i] = [];
+        for (let j = 0; j < columns; j++) {
+            board[i].push(" ");
+        }
+    }
+    console.log(board)
+    //Method for getting entire board for our UI
+    const getBoard = () => board;
+
+
+    const placeMark = (row, column, playerMark) => {
+        if (row < 0 || row > 2 || column < 0 || column > 2 || board[row][column] !== " ") return;
+        board[row][column] = playerMark;
+        console.log(board)
+    };
+
+    const displayBoard = () => {
+        console.log(`
+            ${board[0][0]} | ${board[0][1]} | ${board[0][2]}
+            ---------
+            ${board[1][0]} | ${board[1][1]} | ${board[1][2]}
+            ---------
+            ${board[2][0]} | ${board[2][1]} | ${board[2][2]}
+        `)
+    }
+
+    return { getBoard, placeMark, displayBoard};
+})();
+
+
 function GameController() {
 
 }
+
+//A function that will change the value of the current space in the board
+// function setSpaceValue() {
+//     let value = "";
+
+//     const addMark = (playerMark) => {
+//         value = playerMark;
+//     }
+
+//     const getValue = () => value;
+
+//     return {
+//         addMark,
+//         getValue
+//     };
+// }
